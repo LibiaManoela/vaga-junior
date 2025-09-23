d2f97c31dcab0e017679faf730895b05ed2c0a06
LibiaManoela <libiamanoelas.hentz@gmail.com>
2025-09-22 23:46:12 -0300

    Estrutura inicial do projeto

---
+++ desafio-vaga-junior/src/main/java/com/libia/desafio_merito/DesafioVagaJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.libia.desafio_merito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVagaJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVagaJuniorApplication.class, args);
+	}
+
+}
